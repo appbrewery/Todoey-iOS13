e00a3a42ca3d4e1384183d15132313cfe26b804a
Paola Castro <paolacastropaolacastro@gmail.com>
2020-01-10 10:48:39 -0800

    use NSCoder to save data into a plist

---
+++ Todoey/Controller/TodoListViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArray = [TodoItem]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        print(dataFilePath)
+        loadItems()
+    }
+    
+    //MARK: - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+
+        return cell
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+    
+        saveItems()
+        tableView.reloadData()
+            
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add New Item
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var alertTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new ite", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // when button is pressed i what to print the text
+            let newItem = TodoItem()
+            newItem.title = alertTextField.text!
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (textField) in
+            alertTextField = textField
+            alertTextField.placeholder = "Type your item here"
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - Model Manipulation Methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("encoding threw error: \(error)")
+        }
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([TodoItem].self, from: data)
+            } catch {
+                print("error when trying to retrieve itemes: \(error)")
+            }
+        }
+    }
+    
+}
+
---
+++ Todoey/Model/todo_item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Paola Castro on 1/8/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class TodoItem: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
